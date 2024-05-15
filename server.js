142d52f69ab1b22b0bc82322efa38aa5beb156d3
naz585 <oldskool11218@gmail.com>
2024-05-15 19:14:37 -0400

    ssl

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const pool = new Pool({
     database: process.env.DB_NAME || 'Logging',
     password: process.env.DB_PASSWORD || 'postgres',
     port: process.env.DB_PORT || 5432,
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 // Middleware to parse JSON bodies
